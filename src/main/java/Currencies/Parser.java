8ba26814e4875e082d61b107a4d3b8735464826a
Alexander Antonenko <mercurialf3@gmail.com>
2022-05-31 04:55:15 +0300

    Updating the parsing of current courses

---
+++ src/main/java/Currencies/Parser.java
@@ -0,0 +1,25 @@
+package Currencies;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
+public class Parser {
+    static Document doc;
+    static String value;
+    static double result;
+
+    public static double getActualCurrency(String inputUrl, String inputXpath) {
+        try {
+            doc = Jsoup.connect(inputUrl).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        value = doc.selectXpath(inputXpath).html();
+        value = value.replace(",", ".");
+        result = Double.parseDouble(value);
+
+        System.out.println(result);
+        return result;
+    }
+}
